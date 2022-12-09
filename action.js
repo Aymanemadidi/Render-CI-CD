8e1a4d9b829910391a8e2f957235f2f5365caece
Aymane Elmadidi <aymanelmadidi@gmail.com>
2022-12-09 15:36:24 +0100

    adding comments

--- action.js
+++ action.js
@@ -1,1 +1,20 @@
+import core from "@actions/core";
+import github from "@actions/github";
+
 console.log("From Github Actions!!!");
+async function run() {
+	const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+
+	const octokit = github.getOctokit(GITHUB_TOKEN);
+
+	const { context = {} } = github;
+	const { pull_request } = context.payload;
+
+	await octokit.issues.createComment({
+		...context.repo,
+		issue_number: pull_request.number,
+		body: "Hello from aymane, thanks for your pull request",
+	});
+}
+
+run();
